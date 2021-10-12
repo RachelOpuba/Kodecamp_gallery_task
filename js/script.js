9847129961afd661e857b55eace564594a8a09ba
RachelOpuba <ogbenirach@gmail.com>
2021-10-12 20:56:39 +0100

    More modification

--- js/script.js
+++ js/script.js
@@ -3,10 +3,9 @@ const menu =[
      id: 1,
      title: "Edikainkong",
      price:3500,
-     img: "./images/edikangikong.jpg",
+     img: "images/edikangikong.jpg",
      info: `Edikang Ikong Soup is a highly nutritious, delicious and 
-            savory vegetable soup natively prepared using ‘ugwu’
-            (the native name for fluted pumpkin leaves) and Malabar 
+            savory vegetable soup natively prepared using ‘ugwu’ and Malabar 
             spinach (locally called water leaf in Nigeria).`  
     },      
 
@@ -14,7 +13,7 @@ const menu =[
         id: 2,
         title: "Amala and Ewedu",
         price:2500,
-        img: "./images/amala.jpg",
+        img: "images/amala.jpg",
         info: `Lorem ipsum dolor sit amet consectetur adipisicing 
                 elit. Dignissimos,  Dignissimos, totam? Lorem ipsum 
                 dolor sit amet consectetur adipisicing`
@@ -24,7 +23,7 @@ const menu =[
         id: 3,
         title: "Ofada Rice and Stew",
         price:3000,
-        img: "./images/ofada.jpg",
+        img: "images/ofada.jpg",
         info: `Lorem ipsum dolor sit amet consectetur adipisicing 
                 elit. Dignissimos,  Dignissimos, totam? Lorem ipsum 
                 dolor sit amet consectetur adipisicing`
@@ -34,7 +33,7 @@ const menu =[
         id: 4,
         title: "Afang Soup",
         price:3000,
-        img: "./images/afang.jpg",
+        img: "images/afang.jpg",
         info: `Lorem ipsum dolor sit amet consectetur adipisicing 
                 elit. Dignissimos,  Dignissimos, totam? Lorem ipsum 
                 dolor sit amet consectetur adipisicing`
@@ -44,7 +43,7 @@ const menu =[
         id: 5,
         title: "Banger and Starch",
         price: 3000,
-        img: "./images/banger.jpg",
+        img: "images/banger.jpg",
         info: `Lorem ipsum dolor sit amet consectetur adipisicing 
         elit. Dignissimos,  Dignissimos, totam? Lorem ipsum 
         dolor sit amet consectetur adipisicing`
@@ -64,7 +63,7 @@ const menu =[
         id: 7,
         title: "Fisherman Soup",
         price: 5000,
-        img: "/images/fisherman.jpg",
+        img: "images/fisherman.jpg",
         info: `Lorem ipsum dolor sit amet consectetur adipisicing 
                 elit. Dignissimos,  Dignissimos, totam? Lorem ipsum 
                 dolor sit amet consectetur adipisicing`
@@ -74,7 +73,7 @@ const menu =[
         id: 8,
         title: "Isiewu",
         price: 4000,
-        img: "../images/isiewu.png",
+        img: "images/isiewu.png",
         info: `Lorem ipsum dolor sit amet consectetur adipisicing 
                 elit. Dignissimos,  Dignissimos, totam? Lorem ipsum 
                 dolor sit amet consectetur adipisicing`
@@ -84,7 +83,7 @@ const menu =[
         id: 9,
         title: "Nkwobi",
         price: 6000,
-        img: "./images/nkwobi.jpg",
+        img: "images/nkwobi.jpg",
         info: `Lorem ipsum dolor sit amet consectetur adipisicing 
                 elit. Dignissimos,  Dignissimos, totam? Lorem ipsum 
                 dolor sit amet consectetur adipisicing`
@@ -94,7 +93,7 @@ const menu =[
         id: 10,
         title: "Jollof Rice",
         price:3000,
-        img: "./images/jollof.jpg",
+        img: "images/jollof.jpg",
         info: `Lorem ipsum dolor sit amet consectetur adipisicing 
                 elit. Dignissimos,  Dignissimos, totam? Lorem ipsum 
                 dolor sit amet consectetur adipisicing`
@@ -104,7 +103,7 @@ const menu =[
         id: 11,
         title: "Okro Soup",
         price: 2800,
-        img: "./images/okro.jpg",
+        img: "images/okro.jpg",
         info: `Lorem ipsum dolor sit amet consectetur adipisicing 
                 elit. Dignissimos,  Dignissimos, totam? Lorem ipsum 
                 dolor sit amet consectetur adipisicing`
@@ -114,7 +113,7 @@ const menu =[
         id: 12,
         title: "Pepper Soup",
         price:2900,
-        img: "./images/pepperSoup.jpg",
+        img: "images/pepperSoup.jpg",
         info: `Lorem ipsum dolor sit amet consectetur adipisicing 
                 elit. Dignissimos,  Dignissimos, totam? Lorem ipsum 
                 dolor sit amet consectetur adipisicing`
@@ -128,36 +127,32 @@ menu.forEach(element => {
     // console.log(element)
 })
 
-let menuItems = document.querySelector('.holder');
+let menuItems = document.querySelector('.menu-section');
 let html = ""
 
-
+//console.log(menuItems)
 menu.forEach(e => {
 
     html += 
     `
-    <section class="p-lg-5 p-md-3 holder">
-    
-        <div class="row">
-            <div class="col-12 col-sm-6 
-            col-lg-6 mb-5 card-holder">
-
-                <div class="card bg-dark text-white">
-                    <h5 class="card-title p-3 fw-bold">Price: N${e.price}</h5>
-                    <img src=${e.img} class="card-img-top">
-                    <div class="card-body">
-                        <h5 class="card-title">${e.title}</h5>
-                        <p class="card-text">${e.info}.</p>
-                    </div>
-                </div>
-            </div>
-
-            
-        </div>
-    </section>
+    <div class="menu-card col-12 col-md-6 col-lg-6 card bg-dark text-white px-5 ">
+    <h5 class="card-title py-3">N${e.price}</h5>
+    <div class="menu-item">
+        <img src=${e.img} class="card-img-top">
+    </div>
+    <div class="info">
+        <h4 class="py-3">${e.title}</h4>
+        <p>${e.info}</p>
+    </div>
+    <a href="" class="btn btn-warning ">Add to cart</a>
+</div>              
+       
     `
-    console.log(e)
- menuItems.innerHTML = html
+   // console.log(e)
+  menuItems.innerHTML = html
 })
 
-//console.log(menu.img)
+
+
+
+
